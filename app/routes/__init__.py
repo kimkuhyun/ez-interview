adfcccbb722cb6b5e7bfaf5f07bb8d0d1ba6b1d3
KOSA_js <wltjrdlqns@gmail.com>
2025-11-05 11:01:08 +0900

    [#4] 파일 이름 변경 - doc_agent -> question_agent

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,5 @@
-from .question_routes import uploads_bp
+from .question_routes import question_bp
 from .stream_routes import interview_bp
 from .report_routes import reports_bp
 
-__all__ = ["uploads_bp", "interview_bp", "reports_bp"]
+__all__ = ["question_bp", "interview_bp", "reports_bp"]
